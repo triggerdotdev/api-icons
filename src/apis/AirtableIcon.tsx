e74bd160de39a210cf7aa7409a736af47e38ff5e
Matt Aitken <matt@mattaitken.com>
2023-06-03 17:58:57 +0100

    Added titles to the icons

--- src/apis/AirtableIcon.tsx
+++ src/apis/AirtableIcon.tsx
@@ -6,6 +6,7 @@ export const AirtableIcon = forwardRef<SVGSVGElement, SVGProps>(
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 64 64"
+      title="Airtable"
       {...props}
       ref={ref}
     >
