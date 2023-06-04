a0151177a85350ab2b90148787ad98bdf6793498
Matt Aitken <matt@mattaitken.com>
2023-06-04 18:43:43 +0100

    Fixed Shopify title

--- src/icons/companies/shopify.tsx
+++ src/icons/companies/shopify.tsx
@@ -6,7 +6,7 @@ export const ShopifyIcon = forwardRef<SVGSVGElement, SVGProps>((props, ref) => (
     viewBox="0 0 100 100"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
-    title="Stripe"
+    title="Shopify"
     {...props}
     ref={ref}
   >
