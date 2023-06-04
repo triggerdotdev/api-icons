e1f3bd98632fded62fe25fbd8bb058163a13aece
Matt Aitken <matt@mattaitken.com>
2023-06-04 18:28:41 +0100

    Added Notion

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -6,12 +6,15 @@ import { AsanaIcon } from "./companies/asana";
 export { AsanaIcon };
 import { GitHubDarkIcon, GitHubLightIcon } from "./companies/github";
 export { GitHubDarkIcon, GitHubLightIcon };
-import { SlackIcon } from "./companies/Slack";
+import { NotionIcon } from "./companies/notion";
+export { NotionIcon };
+import { SlackIcon } from "./companies/slack";
 export { SlackIcon };
 
 export const icons: Record<string, Icon> = {
   airtable: { all: AirtableIcon },
   asana: { all: AsanaIcon },
   github: { light: GitHubLightIcon, dark: GitHubDarkIcon, default: "dark" },
+  notion: { all: NotionIcon },
   slack: { all: SlackIcon },
 };
